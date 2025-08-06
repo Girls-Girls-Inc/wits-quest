62843bd3ac78c2bd2cf04586620864493a0cd598
Kayisha naidoo <naidookayisha8@gmail.com>
2025-08-06 22:28:16 +0200

    including CORS

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
-const express = require('express');
+const express = require("express");
 
 const app = express();
-app.use(express.static('frontend/dist'));
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const path = require("path");
+app.use(express.static("frontend/dist"));
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-    console.log("Server Listening on PORT:", PORT);
+  console.log("Server Listening on PORT:", PORT);
 });

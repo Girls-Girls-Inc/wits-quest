ff13e0d61abf2207cc6724024c91cc94aa03a953
froggomelo <146952438+froggomelo@users.noreply.github.com>
2025-08-07 20:21:37 +0200

    Feature branch setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,9 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+const supabase = createClient('https://<project>.supabase.co', '<your-anon-key>')
+
+
 function App() {
   const [count, setCount] = useState(0)
 

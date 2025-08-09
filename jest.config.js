4915280fe5756e68833993370e8c75dca5bd677e
JedK721 <jzkanju@gmail.com>
2025-08-09 16:17:48 +0200

    testing fully set up with sample files

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,14 @@
 module.exports = {
   testEnvironment: "node",
-  roots: ["<rootDir>/frontend/src", "<rootDir>/backend/tests"],
+  roots: ["<rootDir>/backend/tests", "<rootDir>/frontend/src"],
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "backend/**/*.js",
+    "frontend/src/**/*.js",
+    "!backend/tests/**",
+    "!frontend/src/**/*.test.js"
+  ],
   coverageDirectory: "coverage",
   coverageReporters: ["text", "lcov"]
 };
+

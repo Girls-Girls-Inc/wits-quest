350a2a9f7ee1564db8e9e0bfda1e8459bc69cac7
Kayisha naidoo <naidookayisha8@gmail.com>
2025-08-18 01:03:01 +0200

    fix(navbar): Fixing the styling on navbar in mobile and standard view

--- frontend/src/pages/dashboard.jsx
+++ frontend/src/pages/dashboard.jsx
@@ -5,7 +5,11 @@ const Dashboard = () => {
   return (
     <div>
       <h1>Dashboard</h1>
-      <p></p>
+      <br />
+      <p>
+        This is the Home page. You can view ongoing quests and important summary
+        info here.
+      </p>
     </div>
   );
 };
--- frontend/src/pages/quests.jsx
+++ frontend/src/pages/quests.jsx
@@ -3,6 +3,10 @@ const Quests = () => {
   return (
     <div>
       <h1>Quests</h1>
+      <br />
+      <p>
+        This is the quests page. You can view quests and treasure hunts here.
+      </p>
     </div>
   );
 };

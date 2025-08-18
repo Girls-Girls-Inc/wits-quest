d9c5051fd22ea5316f8715a2ddeb837df2b6ebf1
froggomelo <146952438+froggomelo@users.noreply.github.com>
2025-08-18 11:41:12 +0200

    feat: dynamic vars

--- backend/supabase/supabaseClient.js
+++ backend/supabase/supabaseClient.js
@@ -1,4 +1,9 @@
 import { createClient } from '@supabase/supabase-js'
+import dotenv from 'dotenv'
 
-// Create a single supabase client for interacting with your database
-const supabase = createClient('https://xyzcompany.supabase.co', 'public-anon-key')
+dotenv.config()
+
+const supabase = createClient(
+    process.env.SUPABASE_URL,
+    process.env.SUPABASE_SERVICE_ROLE_KEY
+)

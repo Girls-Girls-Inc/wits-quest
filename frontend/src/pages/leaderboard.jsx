8a7c888e8214d7b927185a3aaac19b4327bcfa17
Clare <clarefjcordeiro@gmail.com>
2025-08-21 23:30:25 +0200

    feat: Leaderboard

---
+++ frontend/src/pages/leaderboard.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import leaderboardIMG from "../assets/Signup.png";
+
+const Leaderboard = () => {
+  return (
+    <div>
+      <h1>Leaderboard</h1>
+      <br />
+      <p>
+        This is the Leaderboard page.
+      </p>
+    </div>
+  );
+};
+
+export default Leaderboard;

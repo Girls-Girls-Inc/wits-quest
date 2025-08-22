40df1213489f276672410caf9974a4dd3c4011a4
Clare <clarefjcordeiro@gmail.com>
2025-08-22 16:05:38 +0200

    fix: trying to fix api and frontend leaderboard

--- backend/app.js
+++ backend/app.js
@@ -6,7 +6,7 @@ require('dotenv').config();
 const leaderboardRoutes = require('./routes/leaderboardRoutes');
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 4000;
 
 // Middleware
 app.use(cors());
--- backend/models/leaderboardModel.js
+++ backend/models/leaderboardModel.js
@@ -1,13 +1,14 @@
 const supabase = require('../config/supabaseClient');
 
 const LeaderboardModel = {
-  async getLeaderboard(periodType, start, end, userId) {
+  async getLeaderboard(periodType, start, end, userId, id) {
   let query = supabase
     .from('leaderboard')
     .select('*')
     .order('rank', { ascending: true });
 
   if (periodType) query = query.ilike('periodType', periodType); // case-insensitive
+  if (id) query = query.ilike('id', id.trim()); // case-insensitive
   if (userId) query = query.eq('userId', userId.trim());
   if (start) query = query.gte('periodStart', new Date(start).toISOString());
   if (end) query = query.lte('periodEnd', new Date(end).toISOString());
@@ -18,37 +19,37 @@ const LeaderboardModel = {
   return data || [];
 },
 
-  async addEntry(entry) {
-    const { data, error } = await supabase
-      .from('leaderboard')
-      .insert(entry)
-      .select();
+//   async addEntry(entry) {
+//     const { data, error } = await supabase
+//       .from('leaderboard')
+//       .insert(entry)
+//       .select();
 
-    if (error) throw error;
+//     if (error) throw error;
 
-    return data;
-  },
+//     return data;
+//   },
 
-  async updateScore(id, score, rank) {
-    const { data, error } = await supabase
-      .from('leaderboard')
-      .update({ score, rank })
-      .eq('id', id)
-      .select();
+//   async updateScore(id, score, rank) {
+//     const { data, error } = await supabase
+//       .from('leaderboard')
+//       .update({ score, rank })
+//       .eq('id', id)
+//       .select();
 
-    if (error) throw error;
+//     if (error) throw error;
 
-    return data;
-  },
+//     return data;
+//   },
 
-  async deleteEntry(id) {
-    const { error } = await supabase
-      .from('leaderboard')
-      .delete()
-      .eq('id', id);
+//   async deleteEntry(id) {
+//     const { error } = await supabase
+//       .from('leaderboard')
+//       .delete()
+//       .eq('id', id);
 
-    return { error };
-  }
+//     return { error };
+//   }
 };
 
 module.exports = LeaderboardModel;

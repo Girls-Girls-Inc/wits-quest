b92d0499605d7a4c183fe68c9757b0a093280343
Kayisha naidoo <naidookayisha8@gmail.com>
2025-08-23 21:42:51 +0200

    fix: app.js

--- backend/app.js
+++ backend/app.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-require('dotenv').config();
+const express = require("express");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+require("dotenv").config();
 
-const leaderboardRoutes = require('./routes/leaderboardRoutes');
+const leaderboardRoutes = require("./routes/leaderboardRoutes");
 
 const app = express();
 const PORT = process.env.PORT || 4000;
@@ -13,8 +13,8 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Routes
-app.use('/api/leaderboard', leaderboardRoutes);
+app.use("/api/leaderboard", leaderboardRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });

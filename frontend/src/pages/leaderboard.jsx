d3582bbdcf3cdb81b11e243b47986962620966bb
Kayisha naidoo <naidookayisha8@gmail.com>
2025-08-23 21:05:20 +0200

    importing leaderboard

---
+++ frontend/src/pages/leaderboard.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Leaderboard = () => {
+  return (
+    <div>
+      <h1>Leaderboard</h1>
+      <p>
+        This is the leaderboard page. thios si where the competition happens!
+      </p>
+    </div>
+  );
+};
+
+export default Leaderboard;

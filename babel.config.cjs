a00f118a1d562e69bcfb84aa6fefdab7288123c0
froggomelo <146952438+froggomelo@users.noreply.github.com>
2025-09-12 15:15:07 +0200

    test(questDetails): added questDetails tests

--- babel.config.cjs
+++ babel.config.cjs
@@ -1,6 +1,8 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', { targets: { node: 'current' } }],
-    '@babel/preset-react',
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    ["@babel/preset-react", { runtime: "automatic" }],
   ],
+  plugins: ["babel-plugin-transform-vite-meta-env"],
+  // or "babel-plugin-transform-import-meta" if you prefer that one
 };
